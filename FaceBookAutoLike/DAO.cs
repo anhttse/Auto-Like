18bcace827f763b966b2742676c20f2a4ff3f541
anhttse <anhttse@hotmail.com>
2017-12-20 17:25:40 +0700

    DAO

---
+++ FaceBookAutoLike/DAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceBookAutoLike
+{
+    public class DAO
+    {
+        
+
+        public DAO()
+        {
+        }
+
+        //public Friends GetFriends(string token)
+        //{
+            
+        //}
+    }
+}
