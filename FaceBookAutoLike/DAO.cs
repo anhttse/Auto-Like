d23e979a039884ae54e55e0cc9e7cecd2ddad308
anhttse <anhttse@hotmail.com>
2017-12-20 17:43:08 +0700

    DAO ADO

--- FaceBookAutoLike/DAO.cs
+++ FaceBookAutoLike/DAO.cs
@@ -1,22 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace FaceBookAutoLike
 {
     public class DAO
     {
-        
-
-        public DAO()
-        {
-        }
-
+//        public DAO(string dbSource, string dbName, string user, string pwd)
+//        {
+//        }
         //public Friends GetFriends(string token)
         //{
             
         //}
+
+
+        
+
+        private SqlConnection GetConnection(string dbSource, string dbName, string user, string pwd)
+        {
+            var cnnS = $"Data Source={dbSource};Initial Catalog={dbName};Persist Security Info=True;User ID={user};Password={pwd}";
+            return new SqlConnection(cnnS);
+        }
+
+        private SqlDataReader Reader(SqlConnection sqlC, string queryString)
+        {
+            SqlCommand command = new SqlCommand(queryString, sqlC);
+            return command.ExecuteReader();
+        }
+
+
+        public Posts GetPosts(string token, string dbSource, string dbName, string user, string pwd)
+        {
+            List<Post> lP = new List<Post>();
+
+            using (var cnn = GetConnection(dbSource, dbName, user, pwd) )
+            {
+                
+            }
+        }
+
+
+
     }
 }
